e580cff912faebac8164b8402e1962d083c87667
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-07-14 22:31:05 +0530

    Time: 65 ms (5.39%) | Memory: 74.4 MB (33.43%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,36 +1,23 @@
 class Solution {
-
     int[][] dp = new int[50001][2];
-
     private int maxP(int[] prices, int day, int n, int buy, int fee) {
 
-        if (day >= n) {
-            return 0;
-        }
-
-        if (dp[day][buy] != -1) {
-            return dp[day][buy];
-        }
-
+        if (day >= n) return 0;
+        if (dp[day][buy] != -1)  return dp[day][buy];
         int profit;
-
         if (buy == 1) {
             // Buy
             int consider = maxP(prices, day + 1, n, 0, fee) - prices[day];
             int notConsider = maxP(prices, day + 1, n, 1, fee);
-
             profit = Math.max(consider, notConsider);
         } else {
             // Sell
             int consider = maxP(prices, day + 1, n, 1, fee) + prices[day] - fee;
             int notConsider = maxP(prices, day + 1, n, 0, fee);
-
             profit = Math.max(consider, notConsider);
         }
-
         return dp[day][buy] = profit;
     }
-
     public int maxProfit(int[] prices, int fee) {
 
         int n = prices.length;
