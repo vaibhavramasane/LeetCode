14ddeea76f4bced8f290513ad781eb914a30465d
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-09-13 19:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (82.02%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int f[10] = {0};
+        int result =0;
+        for(auto& d : digits) f[d]++;
+        for(int i=1; i<10; i++){
+            for(int j=0; j<10; j++){
+                for(int k=0; k<9; k+=2){
+                    result +=f[i]>0 && f[j]>(i==j) && f[k]>(i==k)+(j==k);
+                }
+            }
+        }
+        return result;
+        
+    }
+};
