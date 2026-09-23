77b26f95e401e2c88708c832094f70a19813b28c
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2026-09-23 15:32:57 +0530

    Time: 2 ms (91.12%) | Memory: 80.3 MB (80.48%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,21 +1,37 @@
 class Solution {
-    boolean binarySearch(int nums[], int target){
-        int l =0,r=nums.length-1;
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid]==target) return true;
-            else if(nums[mid]<target) l= mid+1;
-            else r = mid-1;
-        }
-        return false;
-    }
+    // boolean binarySearch(int nums[], int target){
+    //     int l =0,r=nums.length-1;
+    //     while(l<=r){
+    //         int mid = l+(r-l)/2;
+    //         if(nums[mid]==target) return true;
+    //         else if(nums[mid]<target) l= mid+1;
+    //         else r = mid-1;
+    //     }
+    //     return false;
+    // }
+    // public int getCommon(int[] nums1, int[] nums2) {
+    //     for(int n : nums1){
+    //         if(binarySearch(nums2,n)){
+    //             return n;
+    //         }
+    //     }
+    //     return -1;
+        
+    // }
+
+    // second approach
     public int getCommon(int[] nums1, int[] nums2) {
-        for(int n : nums1){
-            if(binarySearch(nums2,n)){
-                return n;
-            }
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int i=0,j=0;
+        while(i<m && j< n){
+            if(nums1[i] == nums2[j]) return nums1[i];
+            else if(nums1[i]<nums2[j]) i++;
+            else j++;
         }
         return -1;
-        
     }
+
+
 }
